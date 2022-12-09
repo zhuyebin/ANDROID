7aea21138a3cbcb93c1e31e45a11e34cf523bec2
zhuyebin <117375661+zhuyebin@users.noreply.github.com>
2022-12-09 23:26:17 +0800

    Add files via upload

---
+++ src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mBtnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBtnLogin =findViewById(R.id.btn_login);
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(MainActivity.this,TextViewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
